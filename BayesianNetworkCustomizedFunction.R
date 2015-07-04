52e5b0855e47b2ca6630b9a244f01ca855f839a8
pk873 <pk8735421@gmail.com>
2015-07-05 01:52:56 +0800

    BayesianNetworkCustomizedFunction.R
    The Bayesian network customized functions

---
+++ BayesianNetworkCustomizedFunction.R
@@ -0,0 +1,27 @@
+#Created by SK Pan 2015/07#
+
+#初始化載入套件bnlearn相關套件#
+initialBnlearn <-function(){
+  library(bnlearn)
+  library(Rgraphviz)
+}
+
+#尋找資料在第幾個Interval#
+findIntervalIndex <- function (IntervalSet,Data){
+  RightHandLimitSet<- as.numeric( sub("[^,]*,([^]]*)\\]", "\\1", levels(IntervalSet)))
+  return (findInterval(Data,RightHandLimitSet, all.inside=TRUE)+1)
+}
+
+#查詢資料屬於怎樣的state#
+queryState <- function (IntervalSet,Data){
+  return (levels(IntervalSet)[findIntervalIndex(IntervalSet = IntervalSet,Data = Data)])
+}
+
+#將輸入的證據轉成離散型資料的list給 likelihood weight sampling使用 更改$運算元後屬性即可套用其他應ㄎ用
+inputEvidence <- function(IntervalSet,E1,E2,E3,E4){
+  ES1<-queryState(IntervalSet$Hour,E1)
+  ES2<-queryState(IntervalSet$WeatherTemp,E2)
+  ES3<-queryState(IntervalSet$RelatedHum,E3)
+  ES4<-queryState(IntervalSet$WindSpeed,E4)
+  return (list(Hour=ES1,WeatherTemp=ES2,RelatedHum=ES3,WindSpeed=ES4))
+}
